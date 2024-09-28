ee6196322fc1960ea064e0e97326ce752af57ff1
EgorGu52 <egorgubanov23@gmail.com>
2024-09-28 11:06:41 +0300

    draco sprite + flip

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -15,11 +15,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") *speed, body.velocity.y);
+        float horizontal = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontal*speed, body.velocity.y);
         if (Input.GetKey(KeyCode.Space))
         {
             body.velocity = new Vector2(body.velocity.x,speed); 
         }
+            if (horizontal > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(horizontal < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        
             
     }
 }
